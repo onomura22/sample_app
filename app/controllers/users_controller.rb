0fda0242721952e0e340008f83725c5a46e5a28c
onomura <onomuros@gmail.com>
2018-09-10 08:32:31 +0000

    10.3.1 complite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -43,12 +47,13 @@ class UsersController < ApplicationController
   # beforeアクション
 
   # ログイン済みユーザーかどうか確認
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+   def logged_in_user
+     unless logged_in?
+       store_location
+       flash[:danger] = "Please log in."
+       redirect_to login_url
+     end
+   end
   
   # 正しいユーザーかどうか確認
   def correct_user
