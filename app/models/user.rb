682199beceb6abc2ffab4f38b7a4ec466148af70
onomura <onomuros@gmail.com>
2018-09-21 07:54:46 +0000

    13.1.4 comp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email # メソッド参照に切り替え
   before_create :create_activation_digest # create_activation_digestメソッド
