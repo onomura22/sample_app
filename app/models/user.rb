a8806dc30211cf45433cfe5bce93008c8ff6acb5
onomura <onomuros@gmail.com>
2018-09-25 09:01:58 +0000

    14.3.2 complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,6 +83,16 @@ class User < ApplicationRecord
     Micropost.where("user_id = ?", id)
   end
   
+   # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # ユーザーのステータスフィードを返す
+  def feed
+    Micropost.where("user_id IN (?) OR user_id = ?", following_ids, id)
+  end
+
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
