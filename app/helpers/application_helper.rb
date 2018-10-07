0a2ff67992f9615a9bbadb3a3fe2f33e5968ed35
onomura <onomuros@gmail.com>
2018-10-07 15:00:01 +0000

    191007

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "サッカー情報シェア"
+    base_title = "FOOTBALL.com"
     if page_title.empty?
       base_title
     else
